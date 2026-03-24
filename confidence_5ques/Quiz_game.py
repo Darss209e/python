a0436e2c1fbb01703e17a26abce76bb0820d48eb
darss209e <darss209e@gmail.com>
2026-03-25 00:22:45 +0530

    new

---
+++ confidence_5ques/Quiz_game.py
@@ -0,0 +1,16 @@
+questions = {
+    "capital of  india?": "delhi",
+    "2+2?":"4",
+    "color of sky?":"blue"
+}
+score =0
+
+for question in questions:
+    user_ans=input(question+" ")
+
+    if user_ans.lower() == questions[question]:
+        print("correct")
+        score+=1
+    else:
+        print("wrong!!")
+print("you got", score,"out of",len(questions))
