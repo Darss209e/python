bcd8bbf33ec3b6e6e3742e51206d5834850ad698
Darshan <darss209e@gmail.com>
2026-06-18 22:24:30 +0530

    new

---
+++ que22.py
@@ -0,0 +1,8 @@
+import random
+
+random_number = random.randint(1, 100)
+num = int(input("Guess a number between 1 and 100: "))  
+if num==random_number:
+    print("Congratulations! You guessed the correct number.")   
+else:
+    print("Sorry, that's not the correct number.")  
